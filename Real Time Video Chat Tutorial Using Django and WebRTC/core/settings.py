28d8b8738803f0a92fad7abfaa2b1d941d8de1c2
Satyam Seth <satyam1998.1998@gmail.com>
2024-10-05 00:11:56 +0530

    setup channel layer

--- Real Time Video Chat Tutorial Using Django and WebRTC/core/settings.py
+++ Real Time Video Chat Tutorial Using Django and WebRTC/core/settings.py
@@ -124,3 +124,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Channels
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("127.0.0.1", 6379)],
+#         },
+#     },
+# }

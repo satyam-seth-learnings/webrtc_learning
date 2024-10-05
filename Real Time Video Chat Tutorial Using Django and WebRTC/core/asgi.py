fe2c5d0411481fdacc6c1e24b749c535e07fcda5
Satyam Seth <satyam1998.1998@gmail.com>
2024-10-05 23:30:18 +0530

    add consumer route

--- Real Time Video Chat Tutorial Using Django and WebRTC/core/asgi.py
+++ Real Time Video Chat Tutorial Using Django and WebRTC/core/asgi.py
@@ -11,14 +11,18 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# application = get_asgi_application()
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
 })
